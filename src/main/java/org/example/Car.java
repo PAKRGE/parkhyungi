3335970ca6952a436c73aba2c873a0c3dd616577
hyungipark <phg5459@naver.com>
2024-10-08 00:50:17 +0900

    자동차 경주 리펙토링

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,31 +1,57 @@
 package org.example;
 
+
 import java.util.Random;
 
-public class Car {
-    Random random = new Random();
-    static  int[] moveCount;
+public class Car extends RandomMoveConditon {
+    private final String carName;
+    private int moveCount;
+    private String moveResult;
 
-    public String[] createCar(String carInput) {
-        moveCount = new int[carInput.split(",").length];
-        return carInput.split(",");
+    public Car(String carName, int moveCount, String moveResult) {
+        this.carName = carName;
+        this.moveCount = moveCount;
+        this.moveResult = moveResult;
     }
 
-    public String[] moveCar(String[] carName, String[] advenceResult) {
-        for (int i = 0; i < carName.length; i++) {
-            int advenceCond = random.nextInt(10);
-
-            if(advenceResult[i] == null) {
-                advenceResult[i] = "";
-            }
-            if (advenceCond > 3) {
-                advenceResult[i] = advenceResult[i] + "-";
-
-                moveCount[i] =  moveCount[i] + 1;
-            }
-        }
-
-        return advenceResult;
+    public void setMoveResult(String result) {
+        moveResult = moveResult + result;
     }
+
+    public String getMoveResult() {
+        return moveResult;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void moveCar() {
+        moveCount++;
+    }
+
 
 }
+
+interface MoveConditon {
+    boolean isMove();
+}
+
+class RandomMoveConditon implements MoveConditon {
+    private final Random RANDOM = new Random();
+    final int randomInput = 10;
+
+    @Override
+    public boolean isMove() {
+        if(3 < RANDOM.nextInt(randomInput)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,79 +1,50 @@
 package org.example;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Car car = new Car();
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] nameInput = scanner.next().split(",");
 
-        while (true) {
-            System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-            String[] carName = car.createCar(scanner.next());
-            String[] advencResult = new String[carName.length];
+        List<Car> carList = Arrays.stream(nameInput)
+                .map((String carName) -> new Car(carName,0, ""))
+                .toList();
 
-            System.out.println("시도할 횟수는 몇회인가요?");
-            int tryNum = scanner.nextInt();
-
-            System.out.println("실행 결과");
-            int tryCount = 0;
-            while (tryCount < tryNum) {
-
-                advencResult = car.moveCar(carName, advencResult);
-                int i = 0;
-                while (i < carName.length) {
-
-                    System.out.println(carName[i] + " : " + advencResult[i]);
-                    i++;
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryNum = scanner.nextInt();
+        for(int i =0; i < tryNum; i++) {
+            for (Car car : carList) {
+                if (car.isMove()) {
+                    car.setMoveResult("-");
+                    car.moveCar();
                 }
-                System.out.println("-----------------------------");
-                if (tryCount + 1 == tryNum) {
-                    System.out.println();
-                }
-                tryCount++;
-
+                System.out.println(car.getCarName() + " : " + car.getMoveResult());
             }
+            System.out.println("==============");
+        }
 
-            int[] moveCount = Car.moveCount;
-            int maxMove = findMax(moveCount);
-
-            List<Integer> winnerIndex = findIndex(moveCount,maxMove);
-            String winner = "";
-
-            for(int i = 0; i < winnerIndex.size(); i++) {
-                if (winner.equals("")) {
-                    winner = carName[winnerIndex.get(i)];
+        int winnerMoveCount = 0;
+        String winnerCarName = "";
+        for(Car car : carList) {
+            if(car.getMoveCount() >= winnerMoveCount) {
+                winnerMoveCount = car.getMoveCount();
+            }
+        }
+        for(Car car : carList) {
+            if(car.getMoveCount() == winnerMoveCount) {
+                if (winnerCarName.isEmpty()) {
+                    winnerCarName = car.getCarName();
                 } else {
-                    winner = winner + ", " + carName[winnerIndex.get(i)];
+                    winnerCarName = "%s,%s".formatted(winnerCarName, car.getCarName());
                 }
             }
-
-            System.out.println(winner + "가 최종 우승했습니다.");
-
         }
-
+        System.out.printf("%s가 최종 승리 하였습니다.%n", winnerCarName);
     }
 
-    public static int findMax(int[] arr) {
-        int max = 0;
-        for (int j : arr) {
-            if (j > max) {
-                max = j;
-            }
-        }
-        return max;
-    }
-
-    public static List<Integer> findIndex(int[] arr, int target) {
-        List<Integer> indexes = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                indexes.add(i);
-            }
-        }
-        return indexes;
-    }
 }
